f39db559d0467a8747efe631962915b0975209d5
Gabriel Barahona <gbaralb1@gmail.com>
2022-09-21 21:27:56 -0700

    SETUP

---
+++ gna-budgeting/main/main/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace main
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
